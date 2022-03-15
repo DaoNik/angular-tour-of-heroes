b0da944fc09ee9060b2902277980f29deb6b0013
DaoNik <nikitataranin@gmail.com>
2022-03-15 19:34:20 +0300

    fix: поправил работу пайпа и вид текста

--- src/app/fence-case.pipe.ts
+++ src/app/fence-case.pipe.ts
@@ -5,17 +5,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FenceCasePipe implements PipeTransform {
 
+
   transform(value: string, ...args: unknown[]): string {
-    return value
-      .split(' ')
-      .map((word) => {
-        let newWord = ''
-        for (let i = 0; i < word.length; i++) {
-          newWord += i % 2 === 0 ? word[i].toUpperCase() : word[i]
-        }
-        return newWord;
-      })
-      .join(' ');
+    let newValue = '';
+    let isUpper = false;
+    for (let i = 0; i < value.length; i++) {
+      if (value[i] === ' ') {
+        newValue += ' ';
+        continue;
+      }
+      newValue += isUpper ? value[i] : value[i].toUpperCase();
+      isUpper = !isUpper;
+    }
+    return newValue;
+
+    // return value
+    //   .split(' ')
+    //   .map((word) => {
+    //     let newWord = ''
+    //     for (let i = 0; i < word.length; i++) {
+    //       newWord += i % 2 === 0 ? word[i].toUpperCase() : word[i]
+    //     }
+    //     return newWord;
+    //   })
+    //   .join(' ');
   }
 
 }
