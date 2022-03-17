dbe3ac25a97e335129baf78cbce4a6ddcc4f56d6
DaoNik <nikitataranin@gmail.com>
2022-03-17 19:49:48 +0300

    feed: почти закончил

--- src/app/book/book-routing.module.ts
+++ src/app/book/book-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookContentComponent } from './book-content/book-content.component';
+import { BookPage2Component } from './book-page2/book-page2.component';
+import { BookPage3Component } from './book-page3/book-page3.component';
+import { BookPage4Component } from './book-page4/book-page4.component';
+import { BookPage5Component } from './book-page5/book-page5.component';
+import { BookComponent } from './book/book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'book',
+   component: BookComponent,
+   children: [
+      {path: 'page1', component: BookContentComponent},
+      {path: 'page2', component: BookPage2Component},
+      {path: 'page3', component: BookPage3Component},
+      {path: 'page4', component: BookPage4Component},
+      {path: 'page5', component: BookPage5Component}
+   ]
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BookRoutingModule } from './book-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { BookComponent } from './book/book.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { BookContentComponent } from './book-content/book-content.component';
+import { BookPage2Component } from './book-page2/book-page2.component';
+import { BookPage3Component } from './book-page3/book-page3.component';
+import { BookPage4Component } from './book-page4/book-page4.component';
+import { BookPage5Component } from './book-page5/book-page5.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    BookComponent,
+    PaginationComponent,
+    BookContentComponent,
+    BookPage2Component,
+    BookPage3Component,
+    BookPage4Component,
+    BookPage5Component
+  ],
   imports: [
     CommonModule,
     BookRoutingModule
+  ],
+  exports: [
+    BookComponent
   ]
 })
 export class BookModule { }
